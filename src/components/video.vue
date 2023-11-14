ffcbbbb2d1dcf9b2e9302b5a1d7ef3568a18465e
xiaoqiu1238 <100764979+xiaoqiu1238@users.noreply.github.com>
2023-11-14 16:56:24 +0800

    1114

---
+++ src/components/video.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    naem:"video",
+    data(){
+        return{
+            title:"视频"
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
